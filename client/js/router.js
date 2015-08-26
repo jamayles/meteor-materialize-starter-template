061883e57e89f9e8ac86307b3af76b9852648794
Andrew Mayles <jamayles@gmail.com>
2015-08-26 02:24:10 -0600

    An implementation of the Materialize Starter Template for Meteor

---
+++ client/js/router.js
@@ -0,0 +1,13 @@
+Router.route(
+	'/',
+	{
+		name: 'HomePage',
+		title: 'Starter Template - Materialize',
+		metaNames:
+		{
+			viewport: 'width=device-width, initial-scale=1, maximum-scale=1.0'
+		},
+		layoutTemplate: 'MainLayout',
+		template: 'HomePage'
+	}
+);
